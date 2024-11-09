c320d834e1f49b211a83c203c4bba65ee142b01c
Aditya Gannavarapu <aditya.g@setu.co>
2024-11-09 12:27:18 +0530

    Fix build

--- frontend/src/app/api/fetch-insights/route.ts
+++ frontend/src/app/api/fetch-insights/route.ts
@@ -1,16 +1,9 @@
-import { NextApiRequest } from "next";
 import { NextResponse } from "next/server";
 import axios from "axios";
 import { BACKEND_URL } from "@/app/constants";
 
-// To handle a GET request to /api
-export async function GET(request: NextApiRequest) {
-  // Do whatever you want
-  return NextResponse.json({ message: "Hello World" }, { status: 200 });
-}
-
 // To handle a POST request to /api
-export async function POST(request: NextApiRequest) {
+export async function POST(request: Request) {
   let body = JSON.stringify({
     insight_type: "top_debit",
     month: 6,
